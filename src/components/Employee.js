003c9810fa225e99cde01405998539e39744ef20
prosperAgada <prosperagada@nvit.tech>
2023-07-20 16:10:50 +0100

    edit frontend

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
-
-const Employee = ({ employee, deleteEmployee }) => {
-  const navigate = useNavigate();
-  const editEmployee = (e, id) => {
-    e.preventDefault();
-    navigate(`/updateEmployee/${id}`);
-  };
+const Employee = ({ employee }) => {
+  // const navigate = useNavigate();
+  // const editEmployee = (e, id) => {
+  //   e.preventDefault();
+  //   navigate(`/updateEmployee/${id}`);
+  // };
   return (
     <tr key={employee.id}>
       <td className="text-left px-6 py-4 whitespace-nowrap">
