a86f2deba8c37dc0a9194538295c1832e01f096d
prosperAgada <prosperagada@nvit.tech>
2023-07-20 15:26:25 +0100

    edit frontend

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -19,12 +19,12 @@ const Employee = ({ employee, deleteEmployee }) => {
         <div className="text-sm text-gray-500">{employee.emailId}</div>
       </td>
       <td className="text-right px-6 py-4 whitespace-nowrap font-medium text-sm">
-        <a
+        {/* <a
           onClick={(e) => editEmployee(e, employee.id)}
           className="text-indigo-600 hover:text-indigo-800 px-4 hover:cursor-pointer"
         >
           Edit
-        </a>
+        </a> */}
         {/* <a
           onClick={(e, id) => deleteEmployee(e, employee.id)}
           className="text-red-600 hover:text-red-800 hover:cursor-pointer"
