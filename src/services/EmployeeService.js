2f7cecd5e4583da22c45a4d51f5b1aeaaedae85f
prosperAgada <prosperagada@nvit.tech>
2023-07-21 13:24:23 +0100

    edited dockerfile

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8081/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://16.171.31.124:8082/api/v1/employees";
 class EmployeeService{
     saveEmployee(employee) { 
         return axios.post(EMPLOYEE_API_BASE_URL, employee);
